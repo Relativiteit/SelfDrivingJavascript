d53740af69c73075b08bd960e878500534ba1282
alejo <alejocain@gmail.com>
2023-05-05 19:30:31 +0200

    strip attributes from objects

--- map/map.js
+++ map/map.js
@@ -37,3 +37,6 @@ console.log(names)
 // 2. Get array of all heights
 const heights = characters.map(character => character.height);
 console.log(heights);
+// 3. Get array of objects with jus tname and height properties
+const minifiedRecords = characters.map(characters => ({ name: characters.name, height: characters.height }));
+console.log(minifiedRecords)
