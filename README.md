a0e7482fb4fc161c46d2c967a320fabe50c3bd16
alejo <alejocain@gmail.com>
2023-05-06 11:09:33 +0200

    How to use reduce

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 
 ## Map function
-Learn how to use the Map function. 
+How to use the Map function. 
+
+## Filter functions
+How to use filter functions. 
--- map/reduce.js
+++ map/reduce.js
@@ -1,3 +1,5 @@
+const { Console } = require("console");
+const { cursorTo } = require("readline");
 
 const characters = [
     {
@@ -28,6 +30,13 @@ const characters = [
         eye_color: 'blue',
         gender: 'male',
     },
+    {
+        name: 'Don black man ',
+        height: 215,
+        mass: 120,
+        eye_color: 'brown',
+        gender: 'male',
+    },
 ];
 
 // !! Make sure the height and mass are INTEGERS!!!
@@ -35,7 +44,23 @@ const characters = [
 const totalMass = characters.reduce((acc, cur) => acc + cur.mass, 0);
 console.log(totalMass);
 // You can rename the callback function and starting value of reduce to whatever string you want 
-const simpleMass = characters.reduce((optell, hudig) => optell + hudig.height, 1000);
-console.log(simpleMass);
+// 2. Height of all characters
+const simpleHeight = characters.reduce((optell, hudig) => optell + hudig.height, 1000);
+console.log(simpleHeight);
+// You can give a initial value of a empty object too !
+// 3. Get total number of character ny eye color IMPORTANT
+const charactersByEyeColor = characters.reduce((acc, cur) => {
+    const color = cur.eye_color
+    if (acc[color]) {
+        acc[color] += 1
+    } else {
+        acc[color] = 1
+    }
+    return acc;
+}, {})
+console.log(charactersByEyeColor);
+// 4. Get the total number of characters in all the characters names 
+const totalNameCharacters = characters.reduce((acc, cur) => acc + cur.name.length, 0);
+console.log(totalNameCharacters);
 
 
